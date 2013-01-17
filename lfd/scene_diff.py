c069c9b3320f3c93e56776405bcb374691ac7bc5
Cameron Lee <cameronlee@berkeley.edu>
2013-01-17 12:29:50 -0800

    added scene diff test

--- lfd/scene_diff.py
+++ lfd/scene_diff.py
@@ -5,7 +5,7 @@ DATA_DIR = osp.join(osp.dirname(__file__), "data")
 # demo_clouds and exp_clouds are lists of point clouds, where the point clouds are a list of points
 def get_scene_dist(demo_clouds, exp_clouds):
     #TODO: fill this in
-    raise NotImplementedError
+    return 0
 
 def find_argmin(d):
     min_key = None
@@ -16,15 +16,32 @@ def find_argmin(d):
             min_value = v
     return min_key
 
+def get_clouds_for_demo(verb_data_accessor, demo_name):
+    demo_clouds = []
+    for stage_num in xrange(verb_data_accessor.get_num_stages(demo_name)):
+        demo_stage_info = verb_data_accessor.get_stage_info(demo_name, stage_num)
+        demo_stage_name = demo_name + "-%i"%stage_num
+        demo_stage_data = verb_data_accessor.get_demo_data(demo_stage_name)
+        demo_clouds.append(demo_stage_data["object_cloud"][demo_stage_info.item]["xyz"])
+    return demo_clouds
+
 # verb_info is a list of tuples with the demo_name and demo_info
 # exp_clouds is the list of point clouds for the new scene
-def get_closest_demo(verb_data_accessor, verb, exp_clouds):
+# ignore is a list of demos that shouldn't be returned
+# if return_dists is True, then return the costs mapping (without the ignored demos)
+def get_closest_demo(verb_data_accessor, verb, exp_clouds, ignore=[], return_dists=False):
     verb_info = verb_data_accessor.get_verb_info(verb)
     demo_scene_dists = {}
     for name, info in verb_info:
-        demo_clouds = []
-        for stage_num in xrange(verb_data_accessor.get_num_stages(name)):
-            verb_stage_data = verb_data_accessor.get_demo_data(name)
-            demo_clouds.append(verb_stage_data["object_cloud"][info.item]["xyz"])
+        demo_clouds = get_clouds_for_demo(verb_data_accessor, name)
         demo_scene_dists[name] = get_scene_dist(demo_clouds, exp_clouds)
-    return find_argmin(demo_scene_dists)
+    for ignored_demo in ignore:
+        if demo_scene_dists.has_key(ignored_demo):
+            demo_scene_dists.pop(ignored_demo)
+
+    closest_demo = find_argmin(demo_scene_dists)
+
+    if return_dists:
+        return (closest_demo, demo_scene_dists)
+    else:
+        return closest_demo
---
+++ lfd/test/scene_diff_test.py
@@ -0,0 +1,29 @@
+from jds_utils.colorize import colorize
+from lfd import scene_diff
+from lfd import multi_item_verbs
+
+def report(x, msg=""):
+    if x:
+        print colorize("Pass", "green")
+    else:
+        print colorize("Fail: %s" % (msg), "red")
+
+def test_scene_diff(verb, exp_name, correct_closest_name, data_dir):
+    verb_data_accessor = multi_item_verbs.VerbDataAccessor(test_info_dir=("test/multi_item/multi_item_data/"+data_dir))
+    exp_clouds = scene_diff.get_clouds_for_demo(verb_data_accessor, exp_name)
+    scene_diff_closest_name, scene_diff_dists = scene_diff.get_closest_demo(verb_data_accessor, verb, exp_clouds, ignore=[exp_name], return_dists=True)
+    if correct_closest_name == scene_diff_closest_name:
+        report(True)
+    else:
+        if scene_diff_dists.has_key(correct_closest_name):
+            msg = "Closest demo was %s at distance %f; demo %s had distance %f" % \
+                  (scene_diff_closest_name, scene_diff_dists[scene_diff_closest_name],
+                  correct_closest_name, scene_diff_dists[correct_closest_name])
+        else:
+            msg = "Closest demo was %s at distance %f; demo %s was not found" % \
+                  (scene_diff_closest_name, scene_diff_dists[scene_diff_closest_name])
+        report(False, msg)
+
+if __name__ == "__main__":
+    test_scene_diff("grab", "grab-marker00", "grab-marker450", "grab_marker_l")
+    test_scene_diff("grab", "grab-marker900", "grab-marker450", "grab_marker_l")
