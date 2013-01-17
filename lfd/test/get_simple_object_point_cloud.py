e897a2a39967a1c0722c10861cdbca005dfcbbb1
Cameron Lee <cameronlee@berkeley.edu>
2013-01-17 11:51:00 -0800

    small arg change to script that gets object clouds

--- lfd/test/get_simple_object_point_cloud.py
+++ lfd/test/get_simple_object_point_cloud.py
@@ -14,16 +14,16 @@ def get_args():
     return args
 
 def save_simple_pc(demo_dir, demo_name, stage_num, file_name):
-    verb_data_accessor = multi_item_verbs.VerbDataAccessor(test_info_dir="test/%s"%demo_dir)
+    verb_data_accessor = multi_item_verbs.VerbDataAccessor(test_info_dir="test/multi_item/multi_item_data/%s"%demo_dir)
     stage_info = verb_data_accessor.get_stage_info(demo_name, stage_num)
     stage_data = verb_data_accessor.get_demo_data(stage_info.stage_name)
-    stage_pc = stage_data["object_clouds"][stage_info.item]["xyz"]
+    stage_pc = stage_data["object_cloud"][stage_info.item]["xyz"]
     pc_down = voxel_downsample(stage_pc, .02)
     np.savetxt("%s.pc" % (stage_info.item if file_name is None else file_name), pc_down)
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print "usage: demo directory (starting at test dir), full demo name (found in yaml file), stage number for which item is the target, point cloud file name ('.pc' will be appended)"
+        print "usage: demo directory, full demo name (found in yaml file), stage number for which item is the target, point cloud file name ('.pc' will be appended)"
     else:
         args = get_args()
         save_simple_pc(args.demo_dir, args.demo_name, int(args.stage_num), args.file_name)
